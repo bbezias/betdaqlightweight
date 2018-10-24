d3b16d52fd8b0b6ec6be0ff11e8567e0802e71e4
Benjamin Bezias <benjaminbezias@gmail.com>
2018-10-24 11:00:51 +0200

    v0.0.2

--- betdaqlightweight/client.py
+++ betdaqlightweight/client.py
@@ -5,7 +5,16 @@ from betdaqlightweight.endpoints import readonly, secure
 class Client:
 
     def __init__(self, username='', password='', language='en', application_identifier=None):
+        """
+        Client object carrying the connection information.
 
+        :param username: Betdaq username
+        :type username: str
+        :param password: Betdaq password
+        :type password: str
+        :param language: Language code (2 letters)
+        :return: response of request success.
+        """
         self.url_readonly = 'https://api.betdaq.com/v2.0/ReadOnlyService.asmx?WSDL'
         self.url_secure = 'https://api.betdaq.com/v2.0/Secure/SecureService.asmx?WSDL'
         self.username = username
@@ -14,14 +23,14 @@ class Client:
         self.language = language
         self.application_identifier = application_identifier
         self._read_only_client = SudsClient(self.url_readonly)
-        self.add_headers(False)
+        self._add_headers(False)
         self._secure_client = SudsClient(self.url_secure)
-        self.add_headers(True)
+        self._add_headers(True)
 
         self.readonly = readonly.Endpoint(self._read_only_client)
         self.secure = secure.Endpoint(self._secure_client)
 
-    def add_headers(self, is_secure=True):
+    def _add_headers(self, is_secure=True):
         if is_secure:
             client = self._secure_client
         else:
