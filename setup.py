440e7402a16ced1cac7de7b62136fe557579c686
benjaminbezias@gmail.com <benjaminbezias@gmail.com>
2018-12-02 19:34:23 +0100

    v0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='betdaqlightweight',
-    version='0.0.3',
+    version='0.1.0',
     packages=find_packages(),
     url='https://github.com/bbezias/betdaqlightweight',
     license='GNU',
